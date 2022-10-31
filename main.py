44943f7fd2f4fa36527cf4c8b60681263b9571d2
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2022-10-31 10:54:31 +0100

    create a full alien fleet

---
+++ main.py
@@ -0,0 +1,140 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+from alien import ALien
+
+
+class VoidInvasion:
+
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption('Invasion from the Void')
+        self.ship = Ship(self)
+    # sprites
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self.create_fleet()
+    # additional variables
+        self.var = False
+
+    def run_game(self):
+        #starting gameplay loop
+        while True:
+            self.check_events()
+            self.ship.update()
+            self.bullets.update()
+            self.update_screen()
+
+
+
+
+    def check_events(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_events(event)
+
+    def  check_keydown_events(self, event):
+        if event.key == pygame.K_q:
+            sys.exit()
+
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+
+        if event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+
+
+    # key for quit the game
+        if event.key == pygame.K_q:
+            sys.exit()
+
+    def check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+
+        if event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+
+
+    def update_screen(self):
+        self.screen.fill(self.settings.background_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            if self.var:
+                bullet.var = True
+            else:
+                bullet.var = False
+            bullet.draw_bullet()
+        self.aliens.draw(self.screen)
+
+        pygame.display.flip()
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
+
+    def create_fleet(self):
+        alien = ALien(self)
+        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_alien_x = available_space_x // (2 * alien_width)
+
+    #checking how many rows of aliens fit to the screen
+
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+    # create full fleet of aliens
+
+        for row_number in range(number_rows):
+            for alien_number in range(number_alien_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
+        alien = ALien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = self.alien_width + 2 * self.alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    #create instance of game model and run it
+    ai = VoidInvasion()
+    ai.run_game()
